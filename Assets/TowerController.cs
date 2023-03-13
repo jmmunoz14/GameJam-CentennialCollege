70c0667c68b661f9366d9b63e3b2be7929645081
jmmunoz14 <jm.munoz14@uniandes.edu.co>
2023-03-13 17:21:22 -0400

    Added interactable tower

--- Assets/TowerController.cs
+++ Assets/TowerController.cs
@@ -5,14 +5,36 @@ using UnityEngine;
 public class TowerController : MonoBehaviour
 {
     // Start is called before the first frame update
+
+
+    public Transform availablePosition;
+
     void Start()
     {
-        
+        availablePosition = transform;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        if(collision.gameObject.tag == "AvailableTerrain")
+        {
+            availablePosition = transform;
+            Debug.Log(collision.gameObject.name);
+
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+        else
+        {
+            Debug.Log(collision.gameObject.name);
+
+            transform.position = availablePosition.position;
+        }
     }
 }
