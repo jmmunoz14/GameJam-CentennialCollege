dfbed78dd1e856aa2f64e527cbf8e851c5b78ab7
Atta Jirofty <jirofty76@gmail.com>
2023-03-13 10:34:00 -0400

    Added WaveGenerator with the option to choose the enemy type

--- Assets/Scripts/WaveGenerator.cs
+++ Assets/Scripts/WaveGenerator.cs
@@ -12,6 +12,8 @@ public class WaveGenerator : MonoBehaviour
 
     [SerializeField] private bool isGeneratingWave = true;
 
+    [SerializeField] private BugType bugType = BugType.SimpleBug;
+
     public enum BugType
     {
         SimpleBug,
@@ -36,8 +38,27 @@ public class WaveGenerator : MonoBehaviour
     {
         while (isGeneratingWave)
         {
+            switch (bugType)
+            {
+                case BugType.SimpleBug:
+                    Vector3 rot = bugPrefabs[0].transform.rotation.eulerAngles;
+                    rot = new Vector3(rot.x, rot.y + 180, rot.z);
+                    Instantiate(bugPrefabs[0], enterance.position, bugPrefabs[0].transform.rotation);
+                    //bugPrefabs[0].transform.rotation = Quaternion.Euler(rot);
+                    break;
+                case BugType.ArmoredBug:
+                    Instantiate(bugPrefabs[1], enterance.position, Quaternion.identity);
+                    break;
+                case BugType.ExplosiveBug:
+                    Instantiate(bugPrefabs[2], enterance.position, Quaternion.identity);
+                    break;
+                case BugType.StealthBug:
+                    Instantiate(bugPrefabs[3], enterance.position, Quaternion.identity);
+                    break;
+                default:
+                    break;
+            }
 
-            Instantiate(bugPrefabs[0], enterance.position, Quaternion.identity);
             yield return new WaitForSeconds(nextWaveTimeInSec);
 
         }
