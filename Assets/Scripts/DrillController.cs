f24ca66aab64b5170816b581f7b18c5636659dda
jmmunoz14 <jm.munoz14@uniandes.edu.co>
2023-03-13 12:33:15 -0400

    Added Tools for left hand

---
+++ Assets/Scripts/DrillController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrillController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onAction(bool active)
+    {
+        if (gameObject.activeSelf)
+        {
+            if (active)
+            {
+                Debug.Log("Drill pressed");
+
+            }
+            else
+            {
+                Debug.Log("Drill unpressed");
+            }
+        }
+
+    }
+}
---
+++ Assets/Scripts/SprayController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SprayController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void onAction(bool active)
+    {
+        if (gameObject.activeSelf)
+        {
+            if (active)
+            {
+                Debug.Log("Spray pressed");
+
+            }
+            else
+            {
+                Debug.Log("Spray unpressed");
+            }
+        }
+
+    }
+
+}
