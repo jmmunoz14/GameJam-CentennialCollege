165945b61046c25a95487efdcf9f733888d619e5
jmmunoz14 <jm.munoz14@uniandes.edu.co>
2023-03-13 14:38:16 -0400

    Added Drill Raycast

--- Assets/_Scripts/Hands/DrillController.cs
+++ Assets/_Scripts/Hands/DrillController.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class DrillController : MonoBehaviour
 {
+    public Transform rayCastOrigin;
+
+    bool onActionCalled = false;
+
+    LayerMask tanksMask;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,22 +20,26 @@ public class DrillController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (onActionCalled)
+        {
+            Debug.Log("Drill pressed");
+            Vector3 fwd = rayCastOrigin.TransformDirection(Vector3.forward);
+
+            if (Physics.Raycast(rayCastOrigin.position, fwd, 0.1f, tanksMask))
+                print("There is something in front of the object!");
+
+            Vector3 forward = rayCastOrigin.TransformDirection(Vector3.forward) * 0.1f;
+            Debug.DrawRay(rayCastOrigin.position, forward, Color.cyan);
+
+
+        }
     }
     // Drill destroys armor for tank enemies
     public void onAction(bool active)
     {
         if (gameObject.activeSelf)
         {
-            if (active)
-            {
-                Debug.Log("Drill pressed");
-
-            }
-            else
-            {
-                Debug.Log("Drill unpressed");
-            }
+            onActionCalled = active;
         }
 
     }
